ab4997f8ebf61baf3d8eb53065fae4c45c0bfd75
Gabrieleabu23 <kaidgabriele23@gmail.com>
2023-12-06 03:44:51 +0100

    add : MILESTONE 2 WITH BONUS 3

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+/*
+Milestone 1 - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed, prendendo le informazioni che ci servono dall’array di oggetti che già trovate.
+Milestone 2 - Se clicchiamo sul tasto “Mi Piace” cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+*/
+
+// PARTE DI MILESTONE2
 const posts = [
     {
         "id": 1,
@@ -58,11 +64,10 @@ const posts = [
 
 // MILESTONE 1
 posts.forEach((element) => {
-
-  // destructuring
-  // PIù "variabili" in un unica dichiarazione
-  // const {nome,cognome,professione}=Anagrafica;
-    const newelement=`<div class="post">
+    // destructuring
+    // PIù "variabili" in un unica dichiarazione
+    // const {nome,cognome,professione}=Anagrafica;
+    const newelement = `<div class="post">
     <div class="post__header">
         <div class="post-meta">                    
             <div class="post-meta__icon">
@@ -81,16 +86,53 @@ posts.forEach((element) => {
     <div class="post__footer">
         <div class="likes js-likes">
             <div class="likes__cta">
-                <a class="like-button  js-like-button" href="#" data-postid="1">
+                <a class="like-button  js-like-button" href="#boh" data-postid="${element.id}" >
                     <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                     <span class="like-button__label">Mi Piace</span>
                 </a>
             </div>
             <div class="likes__counter">
-                Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+                Piace a <b id="like-counter-${element.id}" class="js-likes-counter">${element.likes}</b> persone
             </div>
         </div> 
     </div>            
 </div>`;
-    document.getElementById("container").innerHTML+=newelement;
+    console.log(parseInt(element.likes))
+    document.getElementById("container").innerHTML += newelement;
 });
+
+
+// MILESTONE 2 WITH BONUS 3
+const vetLike = [];
+const buttonPremuto = document.querySelectorAll('a[data-postid]');
+buttonPremuto.forEach(element => {
+    element.addEventListener('click',
+        function () {
+            const postId = element.getAttribute('data-postid');
+            const likesz = document.getElementById(`like-counter-${postId}`);
+            let contatore;
+            if (!(vetLike).includes(postId)) {
+                vetLike.push(postId);
+                element.classList.add("like-button--liked")
+                contatore = likesz.textContent;
+                contatore++;
+                likesz.textContent = contatore;
+                // DEBUG
+                // console.log(vetLike);
+            }
+            else if (vetLike.includes(postId)) {
+                vetLike.pop(postId);
+                element.classList.remove("like-button--liked")
+                contatore = likesz.textContent;
+                contatore--;
+                likesz.textContent = contatore;
+                // DEBUG
+                // console.log(vetLike);
+            }
+            console.log("datapost-ID dei Button con mi piace: ", vetLike);
+        }
+    );
+
+});
+
+
